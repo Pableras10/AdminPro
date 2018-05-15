b65ba165ff62eeddfe43bcaa209a9ff5d20914e4
Pableras <zirikoki@hotmail.com>
2018-05-15 11:43:06 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+
+
+// MODULOS
+import { PagesModule } from './pages/pages.module';
+
+
+
+// RUTAS
+import { APP_ROUTES } from './app.routes';
+import { PagesComponent } from './pages/pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES,
+    PagesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesComponent } from './pages/pages.component';
+
+import { Component } from '@angular/core';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
+
+
+
+const appRoutes: Routes = [
+    {path: 'login', component: LoginComponent },
+    {path: 'register', component: RegisterComponent },
+    {path: '**', component: NopagefoundComponent }
+];
+export const APP_ROUTES = RouterModule.forRoot(appRoutes, { useHash: true });
